e2d1ed134830690177588f894489dc703261bf07
Roxanne Weber <marie.sankofa@gmail.com>
2021-12-17 11:51:13 -0600

    logout now complete and working as expected

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,7 +23,6 @@ const Login = () => {
 		axios
 			.post('http://localhost:5000/api/login', cred)
 			.then((res) => {
-				console.log(res);
 				localStorage.setItem('token', res.data.token);
 				push('/view');
 			})
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 const Logout = () => {
-	return (
-		<div>
-			<h2>Logout</h2>
-		</div>
-	);
+	const { push } = useHistory();
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+		console.log(token);
+		axios
+			.post(
+				'http://localhost:5000/api/logout',
+				{},
+				{
+					headers: {
+						authorization: token,
+					},
+				}
+			)
+			.then((res) => {
+				localStorage.removeItem('token');
+				push('/login');
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	return <div></div>;
 };
 
 export default Logout;
